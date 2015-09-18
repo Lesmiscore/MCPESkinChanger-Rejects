43bc740ebd5cda1ca3a366a3db245f286634b8d7
nao20010128nao <nao20010128@gmail.com>
2015-09-18 22:09:46 +0900

    エラーが出る部分を一括削除

--- src/com/nao20010128nao/MCPE/SC/MainActivity.java
+++ src/com/nao20010128nao/MCPE/SC/MainActivity.java
@@ -23,41 +23,17 @@ import com.nao20010128nao.MCPE.SC.plugin.*;
 
 public class MainActivity extends SHablePreferenceActivity {
 	public static WeakReference<MainActivity> instance=new WeakReference<>(null);
-	static boolean preventStart=false;;
-	Map<String,URI> skins=ModificateActivity.skins;
+	static boolean preventStart=false;
 	String changeTmp=null;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 		instance = new WeakReference<MainActivity>(this);
-		if (ModificationService.instance.get() != null) {
-			startActivity(new Intent(this, ModificateActivity.class).putExtra("mode", "noservice"));
-			MainActivity.preventStart();
-		} else
-			startActivity(new Intent(this, SplashActivity.class));
-		if(preventStart){
-			finish();
-			return;
-		}
        	addPreferencesFromResource(R.xml.pref_main);
 		sH("startChange", new OnClickListener(){
 				public void onClick(String p1, String p2, String p3) {
-					if (skins.size() == 0) {
-						new AlertDialog.Builder(MainActivity.this)
-							.setTitle(R.string.err_title)
-							.setCancelable(true)
-							.setMessage(R.string.err_no_skins)
-							.setNegativeButton(android.R.string.ok, new DialogInterface.OnClickListener(){
-								public void onClick(DialogInterface a, int b) {
-									a.cancel();
-								}
-							})
-							.create()
-							.show();
-						return;
-					}
-					startActivity(new Intent(MainActivity.this, ModificateActivity.class).putExtra("mode", "normal"));
+				
 				}
 			});
 		sH("wantUpdate", new OnClickListener(){
@@ -111,91 +87,11 @@ public class MainActivity extends SHablePreferenceActivity {
 		link("selectSilverFish", "assets/images/mob/silverfish.png");
 		link("selectSlime", "assets/images/mob/slime.png");
 		link("selectWitherSkeleton", "assets/images/mob/wither_skeleton.png");
-		
-		sH("inputFrom", new OnClickListener(){
-				public void onClick(String p1, String p2, String p3) {
-					final int revert=Tools.getSettings("input.mode", 0, MainActivity.this);
-					new AlertDialog.Builder(MainActivity.this).
-						setTitle(R.string.inputFrom).
-						setSingleChoiceItems(R.array.inputList, revert, new DialogInterface.OnClickListener(){
-							public void onClick(DialogInterface di, int where) {
-								Tools.setSettings("input.mode", where, MainActivity.this);
-							}
-						}).
-						setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener(){
-							public void onClick(DialogInterface di, int where) {
-								Tools.setSettings("input.mode", revert, MainActivity.this);
-							}
-						}).
-						setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener(){
-							public void onClick(DialogInterface di, int where) {
-								switch (Tools.getSettings("input.mode", 0, MainActivity.this)) {
-									case 0://installed
-										startActivity(new Intent(MainActivity.this, SplashActivity.class));
-										break;
-									case 1://select
-										startActivityForResult(new Intent(Intent.ACTION_GET_CONTENT).setType("application/vnd.android.package-archive"), 456);
-										break;
-								}
-							}
-						}).
-						show();
-				}
-			});
-		sH("outputTo", new OnClickListener(){
-				public void onClick(String p1, String p2, String p3) {
-					final int revert=Tools.getSettings("output.mode", 0, MainActivity.this);
-					new AlertDialog.Builder(MainActivity.this).
-						setTitle(R.string.outputTo).
-						setSingleChoiceItems(R.array.outputList, revert, new DialogInterface.OnClickListener(){
-							public void onClick(DialogInterface di, int where) {
-								Tools.setSettings("output.mode", where, MainActivity.this);
-							}
-						}).
-						setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener(){
-							public void onClick(DialogInterface di, int where) {
-								Tools.setSettings("output.mode", revert, MainActivity.this);
-							}
-						}).
-						setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener(){
-							public void onClick(DialogInterface di, int where) {
-								switch (Tools.getSettings("output.mode", 0, MainActivity.this)) {
-									case 0://install
-										break;
-									case 1://select
-										startActivityForResult(new Intent(Intent.ACTION_CREATE_DOCUMENT).setType("application/vnd.android.package-archive"), 789);
-										break;
-								}
-							}
-						}).
-						show();
-				}
-			});
-		sH("chgList", new OnClickListener(){
-				public void onClick(String p1, String p2, String p3) {
-					startActivity(new Intent(MainActivity.this, ChangingListEditor.class));
-				}
-			});
-		Preference p;
-		(p=findPreference("version")).setSummary(Utils.getVersionName(this));
-		p.setEnabled(false);
-		(p=findPreference("betabuild")).setSummary(Utils.getBetaVersion(this));
-		p.setEnabled(false);
 		sH("openPlayStore",new OnClickListener(){
 				public void onClick(String p1, String p2, String p3) {
 					startActivity(Intent.createChooser(new Intent(Intent.ACTION_VIEW).setData(Uri.parse("http://play.google.com/store/apps/details?id="+getPackageName())),getResources().getString(R.string.openPlayStore)));
 				}
 			});
-		sH("plugins", new OnClickListener(){
-				public void onClick(String p1, String p2, String p3) {
-					startActivity(new Intent(MainActivity.this, PluginLauncher.class));
-				}
-			});
-		try {
-			ImageLoader.startLoadImagesAsync(getApkPath());
-		} catch (PackageManager.NameNotFoundException e) {
-			
-		}
     }
 	void selectFileForSkin(String name){
 		Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
@@ -217,87 +113,6 @@ public class MainActivity extends SHablePreferenceActivity {
 				selectFileForSkin(name);
 			}
 		};
-	}
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		// TODO: Implement this method
-		super.onActivityResult(requestCode, resultCode, data);
-		switch (requestCode) {
-			case 123:
-				if (resultCode == RESULT_OK) {
-					Log.d("dbg", "INTENT:" + data.getDataString());
-					try {
-						skins.put(changeTmp, new URI(data.getDataString()));
-					} catch (URISyntaxException e) {
-
-					}
-					startActivityForResult(new Intent(this, ContentFileLocalCopyActivity.class).
-										   setData(Uri.parse(data.getDataString())).
-										   putExtra("path", changeTmp), 1231);
-				}
-				changeTmp = null;
-				break;
-			case 1231:
-				try {
-					skins.put(data.getStringExtra("path"), new File(data.getStringExtra("result")).toURI());
-				} catch (Throwable e) {
-
-				}
-				break;
-			case 456:
-				if (resultCode == RESULT_OK) {
-					Tools.setSettings("input.where", data.getDataString(), this);
-					startActivityForResult(data.setClass(this, ContentFileLocalCopyActivity.class).putExtra("dest", new File(getFilesDir(), "mcpeCopy_unchecked.apk") + ""), 4561);
-				} else if (Tools.getSettings("input.where", (String)null, this) == null)
-					Tools.setSettings("input.mode", 0, this);
-				break;
-			case 4561:
-				final File unchecked=new File(data.getStringExtra("result"));
-				File checked=new File(getFilesDir(), "mcpeCopy.apk");
-				if(unchecked.exists()){
-					try {
-						AndroidPackage pak=new AndroidPackage(unchecked);
-						PackageInfo info=pak.getResult();
-						if ("com.mojang.minecraftpe".equals(info.packageName)) {
-							unchecked.renameTo(checked);
-						} else {
-							ApplicationInfo appInfo=info.applicationInfo;
-							AndroidPackage.AppSnippet as=AndroidPackage.getAppSnippet(this, appInfo, unchecked);
-							CharSequence s=as.label;
-							String mes=getResources().getString(R.string.fakeapp).replace("@APP@", s);
-							new AlertDialog.Builder(this).
-								setMessage(mes).
-								setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener(){
-									public void onClick(DialogInterface a, int w) {
-										a.cancel();
-										unchecked.delete();
-									}
-								}).
-								show();
-						}
-					} catch (Throwable e) {
-						new AlertDialog.Builder(this).
-							setMessage(R.string.badapk).
-							setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener(){
-								public void onClick(DialogInterface a, int w) {
-									a.cancel();
-									unchecked.delete();
-								}
-							}).
-							show();
-					}
-				} else {
-					return;
-				}
-				Tools.setSettings("input.where", data.getStringExtra("result"), this);
-				break;
-			case 789:
-				if (resultCode == RESULT_OK)
-					Tools.setSettings("output.where", data.getDataString(), this);
-				else if (Tools.getSettings("output.where", (String)null, this) == null)
-					Tools.setSettings("output.mode", 0, this);
-				break;
-		}
 	}
 	public static void preventStart(){
 		preventStart=true;
--- src/com/nao20010128nao/MCPE/SC/plugin/PluginTest.java
+++
@@ -1,6 +0,0 @@
-package com.nao20010128nao.MCPE.SC.plugin;
-import com.nao20010128nao.MCPE.SC.misc.*;
-
-public class PluginTest extends SmartFindViewActivity
-{
-}
