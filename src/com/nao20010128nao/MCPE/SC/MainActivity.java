98008efb0290aaec0c3d24b9d01d931ea226a717
nao20010128nao <nao20010128@gmail.com>
2015-09-18 23:50:29 +0900

    いろいろ

--- src/com/nao20010128nao/MCPE/SC/MainActivity.java
+++ src/com/nao20010128nao/MCPE/SC/MainActivity.java
@@ -7,17 +7,26 @@ import com.nao20010128nao.MCPE.SC.misc.*;
 import com.nao20010128nao.MC_PE.SkinChanger.REJECTED.*;
 import com.nao20010128nao.ToolBox.HandledPreference.*;
 import java.lang.ref.*;
+import com.nao20010128nao.MCPE.SC.plugin.*;
+import java.io.*;
 
 public class MainActivity extends SHablePreferenceActivity {
 	public static WeakReference<MainActivity> instance=new WeakReference<>(null);
 	static boolean preventStart=false;
 	String changeTmp=null;
+	DiffMap<String,byte[]> data;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 		instance = new WeakReference<MainActivity>(this);
        	addPreferencesFromResource(R.xml.pref_main);
+		try {
+			data = PluginUtils.getMapFromIntent(getIntent());
+		} catch (IOException e) {
+			setResult(RESULT_CANCELED);
+			finish();
+		}
 		/*Rejected changes*/
 		
 		/*Rejected changes END*/
@@ -27,19 +36,19 @@ public class MainActivity extends SHablePreferenceActivity {
 				}
 			});
     }
-	void selectFileForSkin(String name){
+	void selectFileForSkin(String name,String mime){
 		Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
 		intent.setType("image/png");
 		changeTmp = name;
 		startActivityForResult(intent, 123);
 	}
-	void link(String pref,String name){
-		sH(pref,createListenerForPerf(name));
+	void link(String pref,String name,String mime){
+		sH(pref,createListenerForPerf(name,mime));
 	}
-	OnClickListener createListenerForPerf(final String name){
+	OnClickListener createListenerForPerf(final String name,final String mime){
 		return new OnClickListener(){
 			public void onClick(String p1, String p2, String p3) {
-				selectFileForSkin(name);
+				selectFileForSkin(name,mime);
 			}
 		};
 	}
