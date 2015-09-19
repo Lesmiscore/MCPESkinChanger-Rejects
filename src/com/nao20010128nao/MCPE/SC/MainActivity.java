7405e6e54aa090a1190af4325ff592a90a242d0f
nao20010128nao <nao20010128@gmail.com>
2015-09-19 10:47:19 +0900

    大体実装

--- src/com/nao20010128nao/MCPE/SC/MainActivity.java
+++ src/com/nao20010128nao/MCPE/SC/MainActivity.java
@@ -9,6 +9,8 @@ import com.nao20010128nao.ToolBox.HandledPreference.*;
 import java.lang.ref.*;
 import com.nao20010128nao.MCPE.SC.plugin.*;
 import java.io.*;
+import android.util.*;
+import java.net.*;
 
 public class MainActivity extends SHablePreferenceActivity {
 	public static WeakReference<MainActivity> instance=new WeakReference<>(null);
@@ -29,7 +31,10 @@ public class MainActivity extends SHablePreferenceActivity {
 			finish();
 		}
 		/*Rejected changes*/
-		
+		link("selectCaveSpider","assets/images/mob/cave_spider.tga",MIME_TGA);
+		link("selectEnderman","assets/images/mob/enderman.tga",MIME_TGA);
+		link("selectSpider","assets/images/mob/spider.tga",MIME_TGA);
+		link("selectTamedWolf","assets/images/mob/wolf_tame.tga",MIME_TGA);
 		/*Rejected changes END*/
 		sH("openPlayStore",new OnClickListener(){
 				public void onClick(String p1, String p2, String p3) {
@@ -52,5 +57,53 @@ public class MainActivity extends SHablePreferenceActivity {
 				selectFileForSkin(name,mime);
 			}
 		};
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// TODO: Implement this method
+		switch(requestCode){
+			case 123:
+				if(resultCode==RESULT_OK){
+					loadInBackground(data.getDataString());
+				}
+				break;
+		}
+	}
+	void loadInBackground(final String path){
+		new Thread(){
+			public void run(){
+				InputStream is=null;
+				ByteArrayOutputStream baos=new ByteArrayOutputStream(1000);
+				try{
+					is=tryOpen(path);
+					byte[] buf=new byte[30000];
+					while(true){
+						int r=is.read(buf);
+						if(r<=0)break;
+						baos.write(buf,0,r);
+					}
+					data.put(path,baos.toByteArray());
+				}catch(Throwable e){
+					e.printStackTrace();
+				}finally{
+					try {
+						is.close();
+					} catch (Throwable e) {
+						
+					}
+				}
+			}
+			InputStream tryOpen(String uri) throws IOException {
+				Log.d("dbg", "tryOpen:" + uri);
+				if (uri.startsWith("content://")) {
+					return getContentResolver().openInputStream(Uri.parse(uri));
+				} else if (uri.startsWith("/")) {
+					return new FileInputStream(uri);
+				} else {
+					return URI.create(uri).toURL().openConnection().getInputStream();
+				}
+			}
+		}.start();
 	}
 }
